30b7d41bcd02d97352305c0d102a6b832e0e2983
Arun Sivarajah <arungsivarajah@gmail.com>
2023-10-01 15:55:08 -0500

    Commiting all changes

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# fetch-rewards-BE
+// README.md
+# Fetch-Rewards-BE
+## Introduction
+This repository hosts the submission by Arun Sivarajah for the Fetch Rewards 2024 Summer Backend Engineering Internship
+### Project Support Features
+* Users can add, spend and view their points balance
+* Public (non-authenticated) users can access all calls on the platform
+### Installation Guide
+* Clone this repository [here](https://github.com/agsiv25/fetch-rewards-BE.git).
+* The main branch is the most stable branch at any given time, ensure you're working from it.
+* Run npm install to install all dependencies
+### Usage
+* Run node . in the terminal to start the application
+* If succesful console will log: Listening on port: http://localhost:8000
+* Connect to the API using preffered method(Postman, Insomnia, Curl) on port 8000
+### API Endpoints
+| HTTP Verbs | Endpoints | Action |
+| --- | --- | --- |
+| POST | /add | Add points to to the users account |
+| POST | /spend | Spend points from the users account |
+| GET | /balance | View user's points balance |
+## POST /add
+### Request
+`POST /add/`
+`curl -d '{ "payer": "DANNON", "points": 300, "timestamp": "2022-10-31T10:00:00Z" }' -H 'Content-Type: application/json' http://localhost:8000/add`
+### Response
+* None
+## POST /spend
+### Request
+`POST /spend/`
+`$ curl -d '{ "points": 5000 }' -H 'Content-Type: application/json' http://localhost:8000/spend`
+### Response
+`[`
+`    {"payer":"DANNON","points":-300},`
+`    {"payer":"UNILEVER","points":-200},`
+`    {"payer":"DANNON","points":200},`
+`    {"payer":"MILLER COORS","points":-4700}`
+`]`
+## GET /balance
+### Request
+`GET /balance/`
+`$ curl http://localhost:8000/balance`
+### Response
+`{`
+`    "DANNON": 1000,`
+`    "UNILEVER": 0,`
+`    "MILLER COORS": 5300`
+`}`
+## Technologies Used
+* [NodeJS](https://nodejs.org/) This is a cross-platform runtime environment built on Chrome's V8 JavaScript engine used in running JavaScript codes on the server. It allows for installation and managing of dependencies and communication with databases.
+* [ExpressJS](https://www.expresjs.org/) This is a NodeJS web application framework.
+## Authors
+* [agsiv25](https://github.com/agsiv25)
+## License
+This project is available for use under the MIT License.
---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+// Define User Data
+let userData = {
+    totalPoints: 0,
+    transactions: [],
+};
+
+app.use(express.json());
+
+//Helper function for post /add
+//Inserts transaction into userData.transactions in chronological order
+function insertTransaction(transaction) {
+    let transactions = userData.transactions;
+    //Convert timestamp to Date object
+    let time = new Date(transaction.timestamp);
+    //If transactions is empty, push transaction
+    if (transactions.length === 0) {
+        transactions.push(transaction);
+        userData.transactions = transactions;
+        return;
+    }
+    //Loop through transactions to find where to insert transaction
+    for (let x = 0; x < transactions.length; x++) {
+        let timestamp = new Date(transactions[x].timestamp);
+        //If transaction is older than current transaction, insert transaction
+        if (time < timestamp) {
+            transactions.splice(x, 0, transaction);
+            userData.transactions = transactions;
+            return;
+        }
+    }
+    transactions.push(transaction);
+    userData.transactions = transactions;
+}
+
+//Helper function for post /spend
+//Spends points in chronological order
+function spend(points) {
+    let transactions = userData.transactions;
+    let spentPoints = [];
+    let index = 0;
+    //Loop through transactions until points is 0
+    while (points > 0) {
+        //If points is greater than current transaction points, subtract points from current transaction points
+        if (transactions[index].points > points) {
+            transactions[index].points -= points;
+            userData.totalPoints -= points;
+            output = 0 - points;
+            spentPoints.push({ payer: transactions[index].payer, points: output });
+            points = 0;
+
+        }
+        //If points is less than current transaction points, subtract current transaction points from points
+        //and increment index to move to next transaction
+        else {
+            points -= transactions[index].points;
+            userData.totalPoints -= transactions[index].points;
+            output = 0 - transactions[index].points;
+            spentPoints.push({ payer: transactions[index].payer, points: output });
+            transactions[index].points = 0;
+            index++;
+        }
+    }
+    userData.transactions = transactions;
+    return spentPoints;
+}
+app.post("/add", (req, res) => {
+    try {
+        const { payer, points, timestamp } = req.body;
+        insertTransaction({ payer, points, timestamp });
+        userData.totalPoints += points;
+
+        res.status(200).send();
+    }
+
+    catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+});
+
+app.post("/spend", (req, res) => {
+    try{
+        const { points } = req.body;
+        //If points is greater than total points, return error
+        if (points > userData.totalPoints) {
+            return res.status(400).json({ error: "Not enough points"});
+        }
+
+        const spentPoints = spend(points);
+        //format output string
+        const output = '[' + spentPoints.map(obj => '\n\t' + JSON.stringify(obj)).join(',') + '\n]';
+
+        res.status(200).send(output);
+
+    }
+    catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+});
+
+app.get("/balance", (req, res) => {
+    try {
+        let payerPoints = new Map();
+        //Loop through transactions and add payer and point to the map
+        for (let transaction of userData.transactions){
+            let {payer, points} = transaction;
+            if (!payerPoints.has(payer)) {
+                payerPoints.set(payer, 0);
+            }
+            payerPoints.set(payer, payerPoints.get(payer) + points);
+        }
+        //format output string
+        const output = '{\n' + Array.from(payerPoints.entries()).map(entry => `\t"${entry[0]}": ${entry[1]}`).join(',\n') + '\n}';
+
+        res.status(200).send(output);
+    }
+    catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+});
+
+app.listen(PORT,() => {
+    console.log(`Listening on port: http://localhost:${PORT}`)
+});
